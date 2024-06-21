7a58c582ade434bf9bd7fd17c011b47080e94daf
Ross L. Mersch (Mike) <merschrossjobs@gmail.com>
2024-06-21 12:41:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process');
 require('dotenv').config();
 
 const client = new Client({
@@ -37,39 +36,7 @@ client.on('messageCreate', async (message) => {
   const command = client.commands.get(commandName);
 
   try {
-    if (commandName === 'add') { // Check if the command is 'add'
-      if (args.length !== 2) {
-        message.reply('**No!** This command requires **exactly** two arguments: ``username password``.');
-        return;
-      }
-
-      const username = args[0];
-      const password = args[1];
-
-      // Execute htpasswd command to add a new user
-      exec(`htpasswd -b /etc/apache2/.htpasswd "${username}" "${password}"`, async (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error executing htpasswd command: ${error}`);
-          message.reply('**Error!** Failed to add user. Please check the bot permissions and try again.');
-          return;
-        }
-
-        // Send a response immediately after executing the command
-        message.channel.send(`I've run the command. I added user "${username}" to the IRS.`);
-        await message.delete(); // Delete the command message after replying
-
-        // Log a console message with the user's ID
-        console.log(`IRS Adder tool ran by ${message.author.tag} (${message.author.id})`);
-        console.log(`Output of htpasswd command:\n${stdout}`);
-
-        // Check if the command produced any error message
-        if (stderr) {
-          console.error(`Error output from htpasswd command:\n${stderr}`);
-        }
-      });
-    } else {
-      await command.execute(message, args); // Execute other commands normally
-    }
+    await command.execute(message, args); // Execute the command based on commandName
   } catch (error) {
     console.error(error);
     message.reply('There was an error executing that command!');
