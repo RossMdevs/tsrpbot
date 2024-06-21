82404c8aeb465c8e7a016824a14d355f58298b03
Ross L. Mersch (Mike) <merschrossjobs@gmail.com>
2024-06-21 13:06:50 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,10 +7,9 @@ module.exports = {
 
     // Help command content
     const helpMessage = `Available commands:
-    ○ **!request <details>** - Streamlines a request to the Board of Directors (Staff+)
+    ○ **!request <details>** - Streamlines a request to the Board of Directors (SIT+)
     ○ **!report <details>** - Report an issue or user (Anyone can use this command)
-    ○ **!role add/remove <user_id/user_mention/username> @role** - Add or remove roles for a user (Authorized Role Only)
-    ○ **!help** - Display this help message.`;
+    ○ **!role add/remove <user_id/user_mention/username> @role** - Add or remove roles for a user (HA+)`;
 
     // Reply to the user with the help message
     message.channel.send(helpMessage)
