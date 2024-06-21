b6c823086ffe9a3b5fad062032c8f092d1fb06a4
Ross L. Mersch (Mike) <merschrossjobs@gmail.com>
2024-06-21 12:47:50 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,9 @@ module.exports = {
   name: 'help',
   description: 'Displays available commands and their usage.',
   execute(message, args) {
-    console.log("testing")
+    // Log a message indicating that the help command logic is being executed
+    console.log("Executing help command logic...");
+
     // Help command content
     const helpMessage = `Available commands:
     ○ **!request <details>** - Streamlines a request to the Board of Directors (Staff+)
@@ -11,6 +13,11 @@ module.exports = {
     ○ **!help** - Display this help message.`;
 
     // Reply to the user with the help message
-    message.channel.send(helpMessage);
+    message.channel.send(helpMessage)
+      .then(() => console.log("Help message sent successfully"))
+      .catch(error => console.error("Error sending help message:", error));
   },
 };
+
+// Log a message to indicate that the help.js file has been read
+console.log("help.js file has been read and initialized.");
