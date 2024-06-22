c96c18aff343929a98289fbaa33d9e441c80f09b
Ross L. Mersch (Mike) <merschrossjobs@gmail.com>
2024-06-22 02:08:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const allowedUserIds = ['760626163147341844'];
 // Define the allowed roles (role IDs) that can use !role command
 const allowedRoleManagerRoles = ['1176929445441982465'];
 
+// Define the roles for requesting and approving additions
+const requestAddRole = '123456789012345678'; // Role ID allowed to request additions
+const approveAddRole = '234567890123456789'; // Role ID allowed to approve additions
+
+// Define the channel ID where approval requests will be posted
+const approvalChannelId = '1253717988675424296';
+
 client.once('ready', () => {
   console.log('TSRP Tool has started.');
 });
@@ -29,14 +36,15 @@ client.on('messageCreate', async message => {
   // Check if the command is "!help"
   if (command === '!help') {
     message.reply(`Available commands:
-    ○ **!add username password** - Adds a user to IRS automatically. (Authorized Users Only)
-    ○ **!role add|remove @user|userID|partialName <role>** - Adds or removes a role from a user (Authorized Users Only).`);
+    ○ **!add username password** - Request to add a user to IRS (Request Add Role Required).
+    ○ **!approve add username password** - Approve an add user request (Approve Add Role Required).
+    ○ **!role add|remove @user|userID|partialName <role>** - Adds or removes a role from a user (Role Manager Role Required).`);
     return;
   }
 
   // Check if the message author's ID is in the allowed list
   if (!allowedUserIds.includes(message.author.id)) {
-    if (command === '!add' || command === '!role') {
+    if (command === '!add' || command === '!role' || command === '!approve') {
       console.log(`Access denied for user: ${message.author.tag} (${message.author.id})`);
       message.reply('**No!**: You are not granted to access this command. This action will be logged.');
     }
@@ -45,22 +53,60 @@ client.on('messageCreate', async message => {
 
   // Check if the command is "!add"
   if (command === '!add') {
+    const member = message.guild.members.cache.get(message.author.id);
+    if (!member.roles.cache.has(requestAddRole)) {
+      console.log(`Unauthorized user attempted !add command: ${message.author.tag} (${message.author.id})`);
+      message.reply('**No!**: You do not have permission to use this command.');
+      return;
+    }
+
     if (args.length !== 2) {
-      message.reply('**No!** This command requires **exactly** two arguments: `username password`.');
+      message.reply('**No!** This command requires **exactly** two arguments: \`username password\`.');
       return;
     }
 
     const username = args[0];
     const password = args[1];
 
+    // Send request for approval to the approval channel
+    const approvalChannel = client.channels.cache.get(approvalChannelId);
+    if (!approvalChannel) {
+      console.error(`Approval channel with ID ${approvalChannelId} not found.`);
+      message.reply('**No!** Approval channel not found.');
+      return;
+    }
+
+    approvalChannel.send(`Request to add user "${username}" by ${message.author.tag}. Use \`!approve add ${username} ${password}\` to approve.`)
+      .then(() => {
+        message.reply('Your request has been submitted for approval.');
+        console.log(`Add request by ${message.author.tag} (${message.author.id}) for user "${username}" has been logged.`);
+        message.delete(); // Delete the command message after logging the request
+      });
+  }
+
+  // Check if the command is "!approve"
+  if (command === '!approve') {
+    const member = message.guild.members.cache.get(message.author.id);
+    if (!member.roles.cache.has(approveAddRole)) {
+      console.log(`Unauthorized user attempted !approve command: ${message.author.tag} (${message.author.id})`);
+      message.reply('**No!**: You do not have permission to use this command.');
+      return;
+    }
+
+    if (args.length !== 3 || args[0] !== 'add') {
+      message.reply('**No!** This command requires the format: \`!approve add username password\`.');
+      return;
+    }
+
+    const username = args[1];
+    const password = args[2];
+
     // Execute htpasswd command to add a new user
     exec(`htpasswd -b /etc/apache2/.htpasswd "${username}" "${password}"`, async () => {
       // Send a response immediately after executing the command
-      message.channel.send(`I've ran the command. I added user "${username}" to the IRS.`);
-      await message.delete(); // Delete the command message after replying
-
+      message.channel.send(`User "${username}" has been added to the IRS by ${message.author.tag}.`);
       // Log a console message with the user's ID
-      console.log(`IRS Adder tool executed by ${message.author.tag} (${message.author.id})`);
+      console.log(`IRS Adder tool executed by ${message.author.tag} (${message.author.id}) for user "${username}".`);
 
       // Execute grep command to search for the username in .htpasswd
       exec(`cat /etc/apache2/.htpasswd | grep "${username}"`, (error, stdout, stderr) => {
@@ -84,7 +130,7 @@ client.on('messageCreate', async message => {
     }
 
     if (args.length < 3) {
-      message.reply('**No!** This command requires at least three arguments: `add|remove @user|userID|partialName <role>`.');
+      message.reply('**No!** This command requires at least three arguments: \`add|remove @user|userID|partialName <role>\`.');
       return;
     }
 
@@ -126,17 +172,17 @@ client.on('messageCreate', async message => {
       }
     } else if (action === 'remove') {
       if (!targetMember.roles.cache.has(role.id)) {
-        message.reply('**No!** User does not have this role.');
+        message.reply('**No!** User does not have this role.`);
       } else {
         targetMember.roles.remove(role)
           .then(() => message.reply(`Role **${role.name}** has been removed from ${targetMember.user.tag}.`))
           .catch(error => {
             console.error(`Error removing role: ${error}`);
-            message.reply('**No!** There was an error removing the role.');
+            message.reply('**No!** There was an error removing the role.`);
           });
       }
     } else {
-      message.reply('**No!** Invalid action. Use `add` or `remove`.');
+      message.reply('**No!** Invalid action. Use \`add\` or \`remove\`.');
     }
   }
 });
